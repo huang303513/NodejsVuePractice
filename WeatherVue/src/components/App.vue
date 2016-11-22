5521e4c7289c05a6bddb2f89bf842bcbfb5d5e3c
huangchengdu <394042583@qq.com>
2016-11-22 17:54:32 +0800

    项目配置完成

---
+++ WeatherVue/src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div >
+	  <div id="app">
+	      <router-view keep-alive>
+	      </router-view>
+	  </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
--- WeatherVue/src/main.js
+++ WeatherVue/src/main.js
@@ -1,8 +1,45 @@
 import Vue from 'vue'
-import App from './App'
+import App from './components/App'
+import Router from 'vue-router'
+import './less/style.less'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+
+var VueResource = require('vue-resource');
+Vue.use(VueResource);
+
+Vue.use(Router);
+var router = new Router({
+    history: false
 })
+
+routerConfig(router);
+router.start(App,'#app');
+window.router = router;
+
+
+function routerConfig(router) {
+	router.map({
+		'/provincelist':{
+			name:'provincelist',
+			component:function(resolve){
+				require(['./components/ProvinceList.vue'],resolve);
+			}
+		},
+		'/hello':{
+			name:'hello',
+			component:function(resolve){
+				require(['./components/Hello.vue'],resolve);
+			}
+		}
+	});
+
+	router.afterEach(function(transition) {
+        // if (transition.to && transition.to.pageId) {
+        //     cUtil.sendUbt(transition.to);
+        // }
+    })
+
+	router.redirect({
+		"*":'/provincelist'
+	});
+}
