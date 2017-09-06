7f4dca30c96b54a6573ca1cd86bf2655f1817040
huangchengdu <394042583@qq.com>
2017-09-06 21:50:44 +0800

    update

--- Node.js核心/http_1.js
+++ Node.js核心/http_1.js
@@ -6,11 +6,22 @@
  */
 var http = require('http')
 
-http.get({
-	hostname:"http://nodejs.cn",
-	port:80,
-	path:'/',
-	agent:false
-},(res)=>{
+// http.get({
+// 	hostname:"http://nodejs.cn/api/",
+// 	port:80,
+// 	path:'/',
+// 	agent:false
+// },(res)=>{
+// 	console.log(res);
+// });
+
+const keepAliveAgent = new http.Agent({ keepAlive: true});
+var options = {
+    hostname: "http://nodejs.cn",
+    port: 80,
+    path: '/',
+    agent: keepAliveAgent
+};
+http.request(options, (res) =>{
 	console.log(res);
 });
