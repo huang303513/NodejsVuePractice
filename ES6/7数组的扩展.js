d1c377277af433fa9732bb8218ae149cfe77a744
huangchengdu <394042583@qq.com>
2017-10-11 13:02:02 +0800

    ES6

---
+++ ES6/7数组的扩展.js
@@ -0,0 +1,112 @@
+//=============================================================
+console.log(...[1,2,3]);
+function add(x,y) {
+	return x + y;
+}
+const numbers = [4, 38];
+console.log(add(...numbers));
+
+function f(v,w,x,y,z) {
+	console.log(arguments);
+}
+const args = [0,1];
+f(-1, ...args, 2, ...[3]);
+//=============================================================
+//代替apply的写法
+console.log(Math.max.apply(null,[14, 3, 77]));
+console.log(Math.max(...[14,3,77]));
+// ES5
+new (Date.bind.apply(Date, [null, 2015, 1, 1]))
+// ES6
+new Date(...[2015, 1, 1]);
+//=============================================================
+//克隆数组
+const a1 = [1, 2];
+const a2 = [...a1];
+const [...a3] = a1;
+console.log(a2,a3);
+//=============================================================
+//合并数组
+let more = [3];
+let origin = [1,2];
+let es5concat = origin.concat(more);
+let es6concat = [...origin,...more];
+console.log(es5concat,es6concat);
+//=============================================================
+//与解构赋值配合使用
+const [first, ...rest] = [1,2,3,4,5];
+console.log(first,rest);
+//=============================================================
+//实现Iterator接口的对象
+let map = new Map([[1,'one'],[2,'two'],[3,'three']]);
+let arr = [...map.keys()];
+console.log(arr);
+//=============================================================
+//from方法把类似数组的对象和可遍历的对象转换为数组
+//类似数组的对象，本质特征只有一点，即必须有length属性。因此，任何有length属性的对象，都可以通过Array.from方法转为数组。
+let arrayLike = {
+	'0':'a',
+	'1':'b',
+	"2":"c",
+	length:3
+};
+let arr1 = [].slice.call(arrayLike);
+let arr2 = Array.from(arrayLike);
+console.log(arr1,arr2);
+//=============================================================
+console.log(Array.from([1,2,3], (x) => x*x))
+//=============================================================
+//将一组值转换为数组
+let array = Array.of(3,11,8);
+console.log(array);
+//=============================================================
+console.log([1,4,-5,10].find((n) => n < 0));
+//=============================================================
+console.log(new Array(3).fill(7))
+//=============================================================
+let ab = ['a','b'];
+for (let index of ab.keys()) {
+	console.log(index);
+}
+// for (let elem of ab.values()) {
+// 	console.log(elem);
+// }
+for (let [index,elem] of ab.entries()) {
+	console.log(index,elem);
+}
+//=============================================================
+console.log([1,2,3].includes(3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ES6/8对象的扩展.js
@@ -0,0 +1,164 @@
+//=============================================================
+let birth = '2000/01/01';
+const Person = {
+    name: '张三',
+    //等同于birth: birth
+    birth,
+    // 等同于hello: function ()...
+    hello() {
+        console.log('我的名字是', this.name);
+    }
+};
+//=============================================================
+//属性的赋值器（setter）和取值器（getter）
+const cart = {
+    _wheels: 4,
+    get wheels() {
+        return this._wheels;
+    },
+    set wheels(value) {
+        console.log(value);
+        if (value < this._wheels) {
+            throw new Error('数值太小');
+        }
+        this._wheels = value;
+    }
+}
+cart.wheels = 5;
+//=============================================================
+let propKey = 'foo';
+
+let obj = {
+    [propKey]: true,
+    ['a' + 'bc']: 123
+};
+//=============================================================
+//函数的name属性
+const person = {
+    sayName() {
+        console.log('hello');
+    }
+}
+console.log(person.sayName.name);
+//=============================================================
+console.log(Object.is('foo', 'foo'));
+console.log(Object.is({}, {}));
+//=============================================================
+//Object.assign方法用于对象的合并，将源对象（source）的所有可枚举属性，复制到目标对象（target）
+const target = { a: 1, b: 4 };
+const source1 = { b: 2 };
+const source2 = { c: 3 };
+Object.assign(target, source1, source2);
+console.log(target);
+//=============================================================
+const v1 = 'abc';
+const v2 = true;
+const v3 = 10;
+const obj0 = Object.assign({}, v1, v2, v3);
+console.log(obj0);
+//=============================================================
+//Object.assign是浅拷贝
+const obj1 = { a: { b: 1 } };
+const obj2 = Object.assign({}, obj1);
+obj1.a.b = 2;
+console.log(obj1.a.b);
+//=============================================================
+//给对象添加属性和方法
+class Point {
+    constructor(x, y) {
+        Object.assign(this, { x, y });
+    }
+}
+Object.assign(Point.prototype, {
+    someMethod(arg1, arg2) {},
+    anotherMethod() {}
+});
+//=============================================================
+//克隆一个对象，包括继承属性
+function clone(origin) {
+    let originProto = Object.getPrototypeOf(origin);
+    return Object.assing(Object.create(originProto), origin);
+}
+//=============================================================
+//合并多个对象
+const merge = (...sources) => { Object.assign({}, ...sources) };
+//=============================================================
+let obj3 = { foo: 123 };
+console.log(Object.getOwnPropertyDescriptor(obj3, 'foo'));
+//=============================================================
+//ES6 规定，所有 Class 的原型的方法都是不可枚举的。
+console.log(Object.getOwnPropertyDescriptor(class { foo() {} }.prototype, 'foo').enumerable);
+//=============================================================
+//ES6新增方法
+const obj4 = {
+    foo: 123,
+    get bar() { return 'abc' }
+}
+console.log(Object.getOwnPropertyDescriptors(obj4));
+//=============================================================
+//手动实现上面那个方法
+function getOwnPropertyDescriptors(obj) {
+    const result = {};
+    for (let key of Reflect.ownKeys(obj)) {
+        result[key] = Object.getOwnPropertyDescriptor(obj, key);
+    }
+    return result;
+}
+console.log('----------', getOwnPropertyDescriptors(obj4));
+//=============================================================
+//__proto__属性（前后各两个下划线），用来读取或设置当前对象的prototype对象
+//__proto__调用的是Object.prototype.__proto__，具体实现如下。
+function isObject(value) {
+    return Object(value) === value;
+}
+Object.defineProperty(Object.prototype, '__proto__', {
+    get() {
+        let _thisObj = Object(this);
+        return Object.getPrototypeOf(_thisObj);
+    },
+    set(proto) {
+        if (this === undefined || this == -null) {
+            throw new TypeError();
+        }
+        if (!isObject(this)) {
+            return undefined;
+        }
+        if (!isObject(proto)) {
+            return undefined;
+        }
+        let status = Reflect.setPrototypeOf(this, proto);
+        if (!status) {
+            throw new TypeError();
+        }
+    }
+});
+//=============================================================
+//Object.setPrototypeOf方法的作用与__proto__相同，用来设置一个对象的prototype对象，返回参数对象本身。
+let proto = {};
+let obj5 = { x: 10 };
+Object.setPrototypeOf(obj5, proto);
+proto.y = 20;
+proto.z = 40;
+console.log(obj5.x, obj5.y, obj5.z);
+//=============================================================
+//Object.getPrototypeOf方法用于读取一个对象的原型对象。
+function Rectangle() {
+}
+const rec = new Rectangle();
+console.log(Object.getPrototypeOf(rec) === Rectangle.prototype);
+Object.setPrototypeOf(rec, Object.prototype);
+console.log(Object.getPrototypeOf(rec) === Rectangle.prototype);
+//=============================================================
+//super关键字总是指向对象的原型
+
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
+//=============================================================
