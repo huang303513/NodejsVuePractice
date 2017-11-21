1007d9c3b74dca4e99cc8fd025c198fb4c2b786f
huangchengdu <394042583@qq.com>
2017-11-21 18:49:51 +0800

    Header布局完成

--- HtmlCssVue/src/components/Header.vue
+++ HtmlCssVue/src/components/Header.vue
@@ -1,6 +1,10 @@
 
 <template>
-  <div>
-   dsfgsdfgs
+  <div id="cm-header">
+    <span class="cm-header-icon fl">
+      <i class="icon-back"></i>
+    </span>
+    <span class="cm-header-btn fr"></span>
+    <h1 class="cm-page-title">携程火车票</h1>
   </div>
 </template>
--- HtmlCssVue/src/pages/App.vue
+++ HtmlCssVue/src/pages/App.vue
@@ -2,23 +2,23 @@
   <div id="wrapper">
     <header-comp></header-comp>
     <keep-alive>
-      <router-view>
+      <router-view class="view">
       </router-view>
     </keep-alive>
   </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue';
-import mixin from '../lib/mixin.js';
+import Header from "../components/Header.vue";
+import mixin from "../lib/mixin.js";
 export default {
-  name: 'app',
-  data(){
-    return {}
+  name: "app",
+  data() {
+    return {};
   },
-  mixin:[mixin],
-  components:{
-    'header-comp':Header
+  mixin: [mixin],
+  components: {
+    "header-comp": Header
   }
-}
+};
 </script>
