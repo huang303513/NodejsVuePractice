141003f3b575097d60ec36d82a7b2cb1b43a699d
huangchengdu <394042583@qq.com>
2017-11-21 10:27:31 +0800

    update

---
+++ HtmlCssVue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    fasdfasdasdfasdfasd
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
