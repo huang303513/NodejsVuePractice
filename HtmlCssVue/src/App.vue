8b76366e01f06777a4d863f3238033fa3d810102
huangchengdu <394042583@qq.com>
2017-11-21 11:06:36 +0800

    添加样式表完成

--- HtmlCssVue/src/App.vue
+++ HtmlCssVue/src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    fasdfasdasdfasdfasd
     <router-view/>
   </div>
 </template>
--- HtmlCssVue/src/router/index.js
+++ HtmlCssVue/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import '../style/style.less'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    }]
 })
